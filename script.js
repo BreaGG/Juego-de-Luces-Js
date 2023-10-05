c5fc1ae0e2402b62d165b63592bb1efc667d211f
BreaG <80957789+BreaGG@users.noreply.github.com>
2023-10-05 11:48:43 +0200

    Añadir contadores y retocar css

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const board = document.getElementById("board");
     const startButton = document.getElementById("start-button");
     const difficultyButtons = document.querySelectorAll('.difficulty input[type="button"]');
+    const attemptsCounter = document.getElementById("attempts");
+    const timerCounter = document.getElementById("timer");
 
     let rows = 5;
     let columns = 6;
     let lights = 10;
+    let attempts = 0;
+    let startTime;
 
     function initializeBoard() {
         board.innerHTML = ""; // Limpia el tablero antes de inicializarlo
@@ -13,22 +17,30 @@ document.addEventListener("DOMContentLoaded", function () {
             for (let j = 0; j < columns; j++) {
                 const button = document.createElement("button");
                 button.className = "board-button";
-                button.addEventListener("click", () => toggleLights(i, j));
+                button.addEventListener("click", () => {
+                    attempts++;
+                    attemptsCounter.textContent = attempts;
+                    toggleLights(i, j);
+                    toggleLights(i - 1, j);
+                    toggleLights(i + 1, j);
+                    toggleLights(i, j - 1);
+                    toggleLights(i, j + 1);
+                    if (checkWin()) {
+                        stopTimer();
+                        alert("¡Has ganado!");
+                    }
+                });
                 board.appendChild(button);
             }
         }
+        attempts = 0;
+        attemptsCounter.textContent = attempts;
+        startTime = new Date().getTime();
         updateBoard();
+        startTimer();
     }
 
     function toggleLights(row, column) {
-        toggleLight(row, column);
-        toggleLight(row - 1, column);
-        toggleLight(row + 1, column);
-        toggleLight(row, column - 1);
-        toggleLight(row, column + 1);
-    }
-
-    function toggleLight(row, column) {
         if (row >= 0 && row < rows && column >= 0 && column < columns) {
             const button = document.getElementsByClassName("board-button")[row * columns + column];
             button.classList.toggle("on");
@@ -44,6 +56,28 @@ document.addEventListener("DOMContentLoaded", function () {
             const randomIndex = Math.floor(Math.random() * (rows * columns));
             buttons[randomIndex].classList.add("on");
         }
+    }
+
+    function checkWin() {
+        const buttons = document.getElementsByClassName("board-button");
+        for (let i = 0; i < buttons.length; i++) {
+            if (!buttons[i].classList.contains("on")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function startTimer() {
+        setInterval(function () {
+            const currentTime = new Date().getTime();
+            const elapsedTime = Math.floor((currentTime - startTime) / 1000);
+            timerCounter.textContent = elapsedTime;
+        }, 1000);
+    }
+
+    function stopTimer() {
+        clearInterval(startTimer);
     }
 
     difficultyButtons.forEach(function (button) {
@@ -74,6 +108,4 @@ document.addEventListener("DOMContentLoaded", function () {
     startButton.addEventListener("click", function () {
         initializeBoard();
     });
-
-    initializeBoard();
 });
