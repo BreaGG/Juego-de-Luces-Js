92dc77fcd455111bf9e96ba06e58f50c1f15ff72
BreaG <80957789+BreaGG@users.noreply.github.com>
2023-10-05 11:39:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const board = document.getElementById("board");
     const startButton = document.getElementById("start-button");
+    const difficultyButtons = document.querySelectorAll('.difficulty input[type="button"]');
 
     let rows = 5;
+    let columns = 6;
     let lights = 10;
 
     function initializeBoard() {
+        board.innerHTML = ""; // Limpia el tablero antes de inicializarlo
         for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < rows; j++) {
+            for (let j = 0; j < columns; j++) {
                 const button = document.createElement("button");
                 button.className = "board-button";
                 button.addEventListener("click", () => toggleLights(i, j));
@@ -26,22 +29,49 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function toggleLight(row, column) {
-        if (row >= 0 && row < rows && column >= 0 && column < rows) {
-            const button = document.getElementsByClassName("board-button")[row * rows + column];
+        if (row >= 0 && row < rows && column >= 0 && column < columns) {
+            const button = document.getElementsByClassName("board-button")[row * columns + column];
             button.classList.toggle("on");
         }
     }
 
     function updateBoard() {
         const buttons = document.getElementsByClassName("board-button");
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove("on");
+        }
         for (let i = 0; i < lights; i++) {
-            const randomIndex = Math.floor(Math.random() * (rows * rows));
+            const randomIndex = Math.floor(Math.random() * (rows * columns));
             buttons[randomIndex].classList.add("on");
         }
     }
 
+    difficultyButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            const custom = button.getAttribute("data-custom");
+            if (custom === "true") {
+                const customRows = parseInt(prompt("Introduce el número de filas:"));
+                const customColumns = parseInt(prompt("Introduce el número de columnas:"));
+                const customLights = parseInt(prompt("Introduce el número de luces:"));
+                if (!isNaN(customRows) && !isNaN(customColumns) && !isNaN(customLights) &&
+                    customRows > 0 && customColumns > 0 && customLights > 0 && customLights < customRows * customColumns) {
+                    rows = customRows;
+                    columns = customColumns;
+                    lights = customLights;
+                    initializeBoard();
+                } else {
+                    alert("Por favor, introduce valores válidos.");
+                }
+            } else {
+                rows = parseInt(button.getAttribute("data-rows"));
+                columns = parseInt(button.getAttribute("data-columns"));
+                lights = parseInt(button.getAttribute("data-lights"));
+                initializeBoard();
+            }
+        });
+    });
+
     startButton.addEventListener("click", function () {
-        board.innerHTML = ""; // Limpia el tablero al hacer clic en el botón de inicio
         initializeBoard();
     });
 
